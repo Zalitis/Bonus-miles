4b7948392631bec0802356895824a81db48604d0
Ilya_Zalitis <zalitis-rabota@mail.ru>
2022-05-23 21:44:29 +0300

    Новый класс

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int stock = 20;
+        int result = cost/stock;
+        return result;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Внесите стоимость билета: ");
         Scanner scan = new Scanner(System.in);
+        BonusMilesService service = new BonusMilesService();
+        System.out.println("Внесите стоимость билета: ");
         int ticketPrice = scan.nextInt();
-        int stock = 20;
-        int bonuses = ticketPrice / stock;
-        System.out.println("У вас " + bonuses + " бонусных миль!");
+        int miles = service.calculate(ticketPrice);
+        System.out.println("У вас " + miles + " бонусных миль!");
     }
 }
 
